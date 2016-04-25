95e9a7048b3c9f548095c8316354e68cc0b0e9ae
Justin Lei <justin.lei@rentrakmail.com>
2016-04-25 09:59:48 -0700

    BUGFIX: Browsers without webworker do not properly fallback to single-threaded mode

--- src/webworker-pool.js
+++ src/webworker-pool.js
@@ -14,37 +14,42 @@
         return self;
     };
 
-    if (typeof(window) === "undefined" || window.Worker === undefined) return WWP;
+    if (typeof(window) === "undefined" || window.Worker === undefined) {
+        WWP.prototype.getWorker = function () { return null; };
+        WWP.prototype.reclaim = function () {};
+    } else {
+        WWP.prototype.getWorker = function (sourceList, onlyReclaimed) {
+            var self    = this,
+                sources = [].concat(sourceList || self._src || []),
+                worker;
 
-    WWP.prototype.getWorker = function (sourceList, onlyReclaimed) {
-        var self    = this,
-            sources = [].concat(sourceList || self._src || []),
-            worker;
+            sources.some(function (src) {
+                var workers = self._workers[src];
+                if (workers === "ignore") return false;
 
-        sources.some(function (src) {
-            var workers = self._workers[src];
-            if (workers === "ignore") return false;
+                worker = workers && workers.length && workers.pop();
+                if (worker) return true;
+                if (onlyReclaimed) return false;
 
-            worker = workers && workers.length && workers.pop();
-            if (worker) return true;
-            if (onlyReclaimed) return false;
+                try {
+                    worker = new Worker(src);
+                    worker.source = src;
 
-            try {
-                worker = new Worker(src);
-                worker.source = src;
+                    self._workers[src] = [];
+                    return true;
+                } catch (error) {
+                    self._workers[src] = "ignore";
+                    return false;
+                }
+            });
 
-                self._workers[src] = [];
-                return true;
-            } catch (error) {
-                self._workers[src] = "ignore";
-                return false;
-            }
-        });
+            return worker;
+        };
 
-        return worker;
-    };
+        WWP.prototype.reclaim = function (worker) {
+            if (worker && worker.source) this._workers[worker.source].push(worker);
+        };
+    }
 
-    WWP.prototype.reclaim = function (worker) {
-        if (worker && worker.source) this._workers[worker.source].push(worker);
-    };
+    return WWP;
 })();
